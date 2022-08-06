e5343cf00b10319d4828c0cfef9703d63816d2c9
NikitaP <unntiss@bk.ru>
2022-08-06 17:22:53 +0300

    Greet

---
+++ Ex_5_Conditions/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username: ");
+string username = Console.Readline();
+
+if (username == "Liza")
+{
+    Console.WriteLine("Hurrah, it's Liza");
+}
+else
+{
+    Console.WriteLine(username);
+}
