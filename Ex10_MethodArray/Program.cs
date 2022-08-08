2f67e001cd9af9a87c4eae78953301dd062e95a5
NikitaP <unntiss@bk.ru>
2022-08-08 15:25:45 +0300

    MethodArray

---
+++ Ex10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 26, 84, 20, 1, 3, 47, 15, 20 };
+
+int n = array.Length;
+int find = 20;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
