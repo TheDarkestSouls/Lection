54e500eae4a35158bffe547a9eafc214d74b49f8
NikitaP <unntiss@bk.ru>
2022-08-08 14:23:30 +0300

    Methods

--- Ex8_IntroMethod/Program.cs
+++ Ex8_IntroMethod/Program.cs
@@ -1,23 +1,26 @@
-﻿int a1 = 25;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 25;
 int b1 = 15;
 int c1 = 56;
-int a2 = 82;
-int b2 = 12;
+int a2 = 3;
+int b2 = 1;
 int c2 = 45;
-int a3 = 74;
+int a3 = 73;
 int b3 = 73;
 int c3 = 19;
 
-int max = a1;
-if(b1 > max) max = b1;
-if(c1 > max) max = c1;
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
 
-if(a2 > max) max = a2;
-if(b2 > max) max = b2;
-if(c2 > max) max = c2;
-
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
